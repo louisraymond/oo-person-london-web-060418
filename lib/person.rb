a007f2a3c2b913f2445fae6409f6bca0e73c6db7
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 17:41:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -43,5 +43,47 @@ def happy?
 
 end
  
+def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      return "blah blah sun blah rain"
+    elsif topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      return "blah blah partisan blah lobbyist"
+    else topic == "other"
+      return "blah blah blah blah blah"
+    end
+  end
+
+end
  
 end
