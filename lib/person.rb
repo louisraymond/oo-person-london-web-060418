a146322c36b8101efc0486e211f5742c12577111
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 17:36:00 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,4 +8,19 @@ class Person
     @happiness=8
     @hygiene_points=8
   end
+  
+  MIN=0
+  MAX=10
+  
+  def happiness=(happiness)
+    @happiness=happiness
+    if @happiness>MAX
+      @happiness=MAX
+    elsif @happiness<MIN
+    @happiness=MIN
+    else
+    @happiness=happiness
+  end
+end
+      
 end
