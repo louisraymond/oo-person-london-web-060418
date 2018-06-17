043adc3f7729c3e2fb519a838dd9b2b6598941ae
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 17:34:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
-# your code goes here
+class Person
+  attr_accessor :bank_account,:happiness, :hygiene_points
+  attr_reader :name
+  
+  def initialize(name)
+    @name=name
+    @bank_account=25
+    @happiness=8
+    @hygiene_points=8
+  end
+end
